71d4008347ea26e675391414c25d0f1bb8322c9e
Braeden Kirby <braedencheetah1234@gmail.com>
2024-09-23 23:55:31 +0000

    Almost Done

--- cosc1010-lab03.py
+++ cosc1010-lab03.py
@@ -1,12 +1,9 @@
-# Your Name Here
+# Braeden Kirby
 # UWYO COSC 1010
-# Submission Date
+# 09/27/24
 # Lab 03 
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Lab Section: 13
+# Sources, people worked with, help given to: Rylan Galloway
 
 
 
@@ -18,43 +15,77 @@ print("Part One-----------------------------------------------------------------
 #We are going to start with the basics. Declare a list  states that contains the elements: Wyoming, Colorado, Montana in that order 
 #Note this is the ONLY point where you need to declare the states list
 
-
+mountain_states = ['Wyoming', 'Colorado', 'Montana']
 
 #print the entire list
 
+print(mountain_states)
 
 #now print the first element in the list
 
+print(mountain_states[0])
 
 #Print the last element using the syntax shown in class to access the final element (hint, think negatives)
 
+print(mountain_states[-1])
 
 #Using an F-string to access the first and second element print the string "COLORADO is south of WYOMING", matching the casing provided
 
-
+print(f'{mountain_states[1].upper()} is south of {mountain_states[0].upper()}')
 
 
 print("Part Two------------------------------------------------------------------------")
 #Append the following states to your list: Washington, Oregon, California and print your list
 
+mountain_states.append('Washington')
+
+mountain_states.append('Oregon')
+
+mountain_states.append('California')
+
+print(mountain_states)
 
 #Again using the specific syntax mentioned in class overwrite the second to last element to be Maine, printing the list 
 
+mountain_states[4] = 'Maine'
+
+print(mountain_states)
 
 #Insert the state Texas to be the third element in the list, again printing your list
 
+mountain_states.insert(2, 'Texas')
+
+print(mountain_states)
 
 #Using the `del` statement remove the fourth item from the list, print your list 
 
+del mountain_states[3]
+
+print(mountain_states)
 
 #Remove Texas using its value, print the list
+
+mountain_states.remove('Texas')
+                
+print(mountain_states)
 
 print("Part Three----------------------------------------------------------------------")
 #Temporarily sort your list, print it both sorted and unsorted 
 
+print(mountain_states)
+
+mountain_states.sort()
+
+print(mountain_states)
 
 #Permanently sort your list in reverse order, printing it out
 
+mountain_states.sort(reverse = True)
+
+print(mountain_states)
 
 #Using the reverse method reverse the list and print it
 
+mountain_states.sort()
+
+print(mountain_states)
